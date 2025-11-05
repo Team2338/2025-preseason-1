9ecf261906910d082f3995b574e2cf1b07677240
Innis Heffernan <inheff62@gmail.com>
2025-11-04 20:36:04 -0600

    Catapult now has basic encoder code

--- src/main/java/team/gif/robot/subsystems/Catapult.java
+++ src/main/java/team/gif/robot/subsystems/Catapult.java
@@ -4,21 +4,35 @@
 
 package team.gif.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
 
 public class Catapult extends SubsystemBase {
     private TalonSRX catapult;
+    private final double catapultLow = 102.67;
+    private final double catapultHigh = 410.67;
     /** Creates a new ExampleSubsystem. */
     public Catapult() {
 catapult = new TalonSRX(RobotMap.CATAPULT_ID);
 catapult.configFactoryDefault();
 catapult.setNeutralMode(NeutralMode.Brake);
+catapult.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+catapult.setSelectedSensorPosition(0);
+catapult.setSensorPhase(true);
+catapult.configForwardSoftLimitThreshold(catapultLow);
+catapult.configReverseSoftLimitThreshold(catapultHigh);
+catapult.configForwardSoftLimitEnable(true);
+catapult.configReverseSoftLimitEnable(true);
     }
 public void turnMotor(double percentoutput){
         catapult.set(TalonSRXControlMode.PercentOutput, percentoutput);
+}
+public void zeroEncoder(){
+        catapult.setSelectedSensorPosition(0);
 }
 }
